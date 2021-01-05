533437c118bf3f91572d00bf02e1ee4353e045c2
J109212104 <jimmy.scada@gmail.com>
2021-01-05 15:03:02 +0800

    20210105

--- database/migrations/2021_01_04_113936_create_haedlines_table.php
+++ database/migrations/2021_01_04_113936_create_haedlines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHaedlinesTable extends Migration
+class CreateHeadlinesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateHaedlinesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('haedlines');
+        Schema::dropIfExists('headlines');
     }
 }
