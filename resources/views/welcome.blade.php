eb474489b26295f79ea8d6961c93f2ee69779c30
J109212104 <jimmy.scada@gmail.com>
2021-01-05 14:51:09 +0800

    20210105

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1>蔡建名的期末網站</h1>
+        <h1>蔡建名的期末網站,最終版</h1>
         <hr>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
